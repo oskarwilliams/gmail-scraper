110eb78ba70cb9f65cae7df70a31a4dad0d7704f
Oskar Williams <oskarwilliams378@gmail.com>
2024-06-09 21:53:52 +0100

    feat: Make work for multiple emails and save to csv

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
 
-export const EMAIL = process.env.EMAIL ?? "";
+export const EMAILS = (process.env.EMAILS ?? "").split(",");
 export const AFTER_DATE = process.env.AFTER_DATE ?? "";
 export const BEFORE_DATE = process.env.BEFORE_DATE ?? "";
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-import { AFTER_DATE, BEFORE_DATE, EMAIL } from "../config.js";
+import { json2csv } from "json-2-csv";
+import { AFTER_DATE, BEFORE_DATE, EMAILS } from "../config.js";
 import { scrapeMetricsForUser } from "./scrape-metrics.js";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
 
 const main = async () => {
   const afterDate = AFTER_DATE;
   const beforeDate = BEFORE_DATE;
-  const userId = EMAIL;
 
-  const results = await scrapeMetricsForUser(userId, beforeDate, afterDate);
-  console.log(results);
+  const resultsPromise = EMAILS.map(
+    async (userId) => await scrapeMetricsForUser(userId, beforeDate, afterDate),
+  );
+
+  const results = await Promise.all(resultsPromise);
+  const csv = json2csv(results);
+
+  const dataDir = "./data";
+
+  if (!existsSync(dataDir)) {
+    mkdirSync(dataDir);
+  }
+  writeFileSync(dataDir + "/results.csv", csv);
 };
 
 main();
