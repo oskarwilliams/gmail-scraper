c9b4f9ca46702eecb93f09e15a757da069059d41
Oskar Williams <oskarwilliams378@gmail.com>
2024-06-10 19:47:40 +0100

    feat: Save to one csv

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,14 @@ import { FROM_DATES, TO_DATES, EMAILS } from "./config.js";
 import { scrapeMetricsForUser } from "./scrape-metrics.js";
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 
-const scrapeAndSave = async (fromDate: string, toDate: string) => {
+const scrape = async (fromDate: string, toDate: string) => {
   const resultsPromise = EMAILS.map(
     async (userId) => await scrapeMetricsForUser(userId, fromDate, toDate),
   );
 
   const results = await Promise.all(resultsPromise);
-  const csv = json2csv(results);
-
-  const dataDir = "./data";
-
-  if (!existsSync(dataDir)) {
-    mkdirSync(dataDir);
-  }
-  writeFileSync(`${dataDir}/results-${fromDate}-${toDate}.csv`, csv);
-  console.log(`Saved results for ${fromDate} to ${toDate}`);
+  console.log(`Scraped results for ${fromDate} to ${toDate}`);
+  return results;
 };
 
 const main = async () => {
@@ -26,10 +19,26 @@ const main = async () => {
   if (fromDates.length !== toDates.length) {
     throw new Error("FROM_DATES and TO_DATES must have the same length");
   }
+  const firstFromDate = fromDates[0];
+  const lastToDate = toDates[toDates.length - 1];
+
+  const results = [];
 
   for (let i = 0; i < fromDates.length; i++) {
-    await scrapeAndSave(fromDates[i], toDates[i]);
+    results.push(...(await scrape(fromDates[i], toDates[i])));
   }
+  const csv = json2csv(results);
+
+  const dataDir = "./data";
+
+  if (!existsSync(dataDir)) {
+    mkdirSync(dataDir);
+  }
+  writeFileSync(`${dataDir}/results-${firstFromDate}-${lastToDate}.csv`, csv);
+  console.log(
+    "Results saved to",
+    `${dataDir}/results-${firstFromDate}-${lastToDate}.csv`,
+  );
 };
 
 main();
